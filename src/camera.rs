1849a29036d7fb1432a7955008db593d1cdd8034
Hugo Bacelar <hugo.bacelar@blip.pt>
2022-05-21 23:24:38 +0100

    chap 11.2

--- src/camera.rs
+++ src/camera.rs
@@ -1,6 +1,9 @@
 use std::f64::consts::PI;
 
-use crate::{ray::Ray, vec::Vec3};
+use crate::{
+    ray::Ray,
+    vec::{self, Vec3},
+};
 
 pub struct Camera {
     origin: Vec3,
@@ -9,29 +12,28 @@ pub struct Camera {
     lower_left_corner: Vec3,
 }
 
-const ASPECT_RATIO: f64 = 16.0 / 9.0;
-
 impl Camera {
-    pub fn new(vfov: f64, aspect_ratio: f64) -> Self {
+    pub fn new(lookfrom: Vec3, lookat: Vec3, vup: Vec3, vfov: f64, aspect_ratio: f64) -> Self {
         let theta = vfov * PI / 180.0;
         let h = (theta / 2.0).tan();
 
         let viewport_height = 2.0 * h;
         let viewport_width = (aspect_ratio * viewport_height).round();
-        let focal_length = 1.0;
 
-        let origin = Vec3(0.0, 0.0, 0.0);
-        let horizontal = Vec3(viewport_width, 0.0, 0.0);
-        let vertical = Vec3(0.0, viewport_height, 0.0);
+        let w = vec::unit_vec(lookfrom - lookat);
+        let u = vec::unit_vec(vec::cross(&vup, &w));
+        let v = vec::cross(&w, &u);
+
+        let origin = lookfrom;
+        let horizontal = u * viewport_width;
+        let vertical = v * viewport_height;
+        let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - w;
 
         Camera {
             origin,
             horizontal,
             vertical,
-            lower_left_corner: origin
-                - horizontal / 2.0
-                - vertical / 2.0
-                - Vec3(0.0, 0.0, focal_length),
+            lower_left_corner,
         }
     }
 
