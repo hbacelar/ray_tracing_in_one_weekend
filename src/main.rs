5cae0170db483423fb997e29f4cef9d2c1b9695f
Hugo Bacelar <hugo.bacelar@blip.pt>
2025-07-05 11:38:45 +0100

    chap 14 final scene

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,66 @@ use ray_tracing_in_one_weekend::{
 
 fn main() {
     // World
+    let mut materials: Vec<MaterialKind> = Vec::new();
 
-    let mat_ground = MaterialKind::Lambertian(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let mat_center = MaterialKind::Lambertian(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    let mat_left = MaterialKind::Dielectric(Dielectric::new(1.50));
-    let mat_bubble = MaterialKind::Dielectric(Dielectric::new(1.00 / 1.50));
-    let mat_right = MaterialKind::Metal(Metal::new(Color::new(0.8, 0.5, 0.2), 1.0));
+    let mut world_centers: Vec<Point> = Vec::new();
 
-    let world = vec![
-        Sphere::new(Point::new(0.0, -100.5, -1.0), 100.0, &mat_ground),
-        Sphere::new(Point::new(0.0, 0.0, -1.2), 0.5, &mat_center),
-        Sphere::new(Point::new(-1.0, 0.0, -1.0), 0.5, &mat_left),
-        Sphere::new(Point::new(-1.0, 0.0, -1.0), 0.4, &mat_bubble),
-        Sphere::new(Point::new(1.0, 0.0, -1.0), 0.5, &mat_right),
-    ];
+    for a in -11..11 {
+        for b in -11..11 {
+            let chose_mat: f64 = rand::random();
+            let center = Point::new(
+                a as f64 + 0.9 * rand::random::<f64>(),
+                0.2,
+                b as f64 + 0.9 * rand::random::<f64>(),
+            );
+
+            if (center - Point::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                world_centers.push(center);
+                if chose_mat < 0.8 {
+                    // diffuse
+                    let albedo = Vec3::random() * Vec3::random();
+                    materials.push(MaterialKind::Lambertian(Lambertian::new(albedo.into())));
+                } else if chose_mat < 0.95 {
+                    // metal
+                    let albedo = Vec3::random_range(0.5, 1.0);
+                    let fuz: f64 = rand::random_range(0.0..0.5);
+                    materials.push(MaterialKind::Metal(Metal::new(albedo.into(), fuz)));
+                } else {
+                    // glass
+                    materials.push(MaterialKind::Dielectric(Dielectric::new(1.5)));
+                }
+            }
+        }
+    }
+
+    let mut world: Vec<Sphere<'_, MaterialKind>> = Vec::new();
+    // ground
+    let ground_material = MaterialKind::Lambertian(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
+    world.push(Sphere::new(
+        Point::new(0.0, -1000.0, 0.0),
+        1000.0,
+        &ground_material,
+    ));
+
+    // big 3
+    let glass = MaterialKind::Dielectric(Dielectric::new(1.5));
+    world.push(Sphere::new(Point::new(0.0, 1.0, 0.0), 1.0, &glass));
+    let diffuse = MaterialKind::Lambertian(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
+    world.push(Sphere::new(Point::new(-4.0, 1.0, 0.0), 1.0, &diffuse));
+    let metal = MaterialKind::Metal(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
+    world.push(Sphere::new(Point::new(4.0, 1.0, 0.0), 1.0, &metal));
+
+    //rest
+    for (idx, center) in world_centers.iter().enumerate() {
+        let mat = materials
+            .get(idx)
+            .expect("materials and centers are not the same size");
+        world.push(Sphere::new(*center, 0.2, mat));
+    }
 
     // Camera
     let aspect_ratio: f64 = 16.0 / 9.0;
-    let image_width: u32 = 800;
+    let image_width: u32 = 400;
 
     let cam = CameraBuilder::new()
         .image_width(image_width)
@@ -33,11 +75,11 @@ fn main() {
         .samples_per_pixel(100)
         .max_depth(50)
         .vfov(20.0)
-        .lookfrom(Point::new(-2.0, 2.0, 1.0))
-        .lookat(Point::new(0.0, 0.0, -1.0))
+        .lookfrom(Point::new(13.0, 2.0, 3.0))
+        .lookat(Point::new(0.0, 0.0, 0.0))
         .vup(Vec3::new(0.0, 1.0, 0.0))
-        .defocus_angle(10.0)
-        .focus_dist(3.4)
+        .defocus_angle(0.6)
+        .focus_dist(10.0)
         .build();
 
     cam.render(&world);
